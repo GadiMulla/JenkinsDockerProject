1a870b96902e6a46b5aa11148e3fe8a0f1097385
GadiMulla <121053177+GadiMulla@users.noreply.github.com>
2023-08-21 21:07:47 +0300

    Update docker_check.sh

--- docker_check.sh
+++ docker_check.sh
@@ -3,9 +3,9 @@
 CONTAINER_NAME="jenkinsdockerproject"
 sleep 20
 
-if docker inspect -f '{{.State.Status}}' "$CONTAINER_NAME" | grep -q "exited"; >
+if docker inspect -f '{{.State.Status}}' "$dockerjenkinsproject" | grep -q "exited"; >
     echo "Success"
 else
     echo "Failed! Container still running"
-    docker kill "$CONTAINER_NAME"
+    docker kill "$dockerjenkinsproject"
 fi
